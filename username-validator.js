e41273fc33543c40b9b981c9e4e2094244d15596
Danilo Santana <danilo@danilo-Ubuntu-22.04>
2024-09-17 21:41:51 -0300

    V.1.0.0 - Includes username-validator

--- username-validator.js
+++ username-validator.js
@@ -1,7 +1,27 @@
 const _username = Symbol('username');
 
 class UsernameValidator{
-    constructor(username){
+
+    constructor(username, minlength=3, maxlength=20){
         this[_username] = username.trim();
+        this.minlength = minlength;
+        this.maxlength = maxlength;
     }
+
+    get username(){
+        return this[_username];
+    }
+
+    usernameIsValid(){
+
+        const usernameRegex = /^[a-zA-z0-9]{this.minlength,this.maxlength}$/;
+
+        if (!usernameRegex.test(this[_username])){
+            return false;
+        }
+
+        return true;
+
+    }
+
 }
